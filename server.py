a9b65218afd8fd95f2199e944b4d8eeaacf31ac9
Michel Weitbrecht <>
2019-08-11 23:21:36 +0200

    Set correct host and user for docker

--- server.py
+++ server.py
@@ -16,9 +16,9 @@ AREA_TYPES_DOCUMENT_FILE = "../area-types/area_types.json"
 AREA_TYPES_SCHEMA_FILE = "../area-types/area_types_schema.json"
 
 # Database settings
-DATABASE_HOST = ""
+DATABASE_HOST = "trump-postgis"
 DATABASE_NAME = "gis"
-DATABASE_USER = "postgres"
+DATABASE_USER = "osm"
 DATABASE_PASSWORD = None
 
 # Resource paths to register the http handlers on
